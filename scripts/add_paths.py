a7660d345bbae589883c196c309151bf760d5c5a
Notexe <josh@notex.app>
2023-10-17 23:20:38 +1000

    Check to avoid replacing paths with hints

--- scripts/add_paths.py
+++ scripts/add_paths.py
@@ -15,8 +15,11 @@ def update_json(hash_val, path_val, hash_type):
             if entry["hash"] == hash_val:
                 if ioi_hash(path_val) == hash_val:
                     entry["path"] = path_val.lower()
-                    entry.pop("hint", None)  
+                    entry.pop("hint", None)
                 else:
+                    if "path" in entry and entry["path"] != "":
+                        print(f"Hash: {hash_val} already has a path {entry['path']}. Skipping addition of hint: {path_val.lower()}.")
+                        continue
                     entry["hint"] = path_val.lower()
                 break
 
