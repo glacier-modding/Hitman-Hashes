5686fd1ee6f04f042e22f676b7ddf209a2799302
Notexe <josh@notex.app>
2023-10-18 15:25:44 +1000

    Add game argument to merge.py

--- scripts/merge.py
+++ scripts/merge.py
@@ -5,7 +5,11 @@ from common_functions import *
 
 parser = argparse.ArgumentParser(description="Merge JSON files and generates a hash list.")
 parser.add_argument('version', type=int, help="Current version number to be embedded in the hash list.")
+parser.add_argument('--game', choices=GAME_FLAGS.keys(), nargs='*', help="Game to generate the hash list for. Defaults to all.")
 args = parser.parse_args()
+
+if not args.game:
+    args.game = list(GAME_FLAGS.keys())
 
 input_directory = "paths"
 
@@ -20,6 +24,8 @@ for json_file in json_files:
     with open(json_file, "r") as f:
         data = json.load(f)
         for entry in data:
+            if entry.get('gameFlags') is None or not any(entry['gameFlags'] & GAME_FLAGS[game] for game in args.game):
+                continue
             if entry.get('path'):
                 path = entry['path']
             elif entry.get('hint'):
@@ -34,7 +40,7 @@ for json_file in json_files:
             merged_entry = f"{entry['hash']}.{os.path.basename(json_file).split('.')[0]},{path}"
             merged_data.append(merged_entry)
 
-completion_percentage = (matching_hashes / total_hashes) * 100
+completion_percentage = (matching_hashes / total_hashes) * 100 if total_hashes else 0
 
 comments = [
     f"# Hash list completion: {completion_percentage:.4f}%",
