ebd35728ce29aca76457fc1cdd123959b4594427
Notexe <josh@notex.app>
2023-10-18 14:59:18 +1000

    Speed up add_new_hashes.py

--- scripts/add_new_hashes.py
+++ scripts/add_new_hashes.py
@@ -4,47 +4,40 @@ from common_functions import *
 
 output_directory = "paths"
 
-def update_json(hash_val, hash_type, game_flags_str):
-    game_flags = 0
-    for flag_str in game_flags_str:
-        game_flags |= GAME_FLAGS[flag_str]
+def update_data(data, hash_val, game_flags):
+    if hash_val not in data:
+        data[hash_val] = {"hash": hash_val, "path": "", "gameFlags": 0}
+    data[hash_val]["gameFlags"] |= game_flags
 
-    json_filename = os.path.join(output_directory, f"{hash_type}.json")
+def read_json_file(filename):
+    if os.path.exists(filename):
+        with open(filename, 'r') as f:
+            entries = json.load(f)
+            return {entry["hash"]: entry for entry in entries}
+    return {}
 
-    data = []
-    if os.path.exists(json_filename):
-        with open(json_filename, 'r') as f:
-            data = json.load(f)
+def write_json_file(filename, data):
+    with open(filename, 'w', newline='\n') as f:
+        json.dump(list(data.values()), f, indent=2)
 
-        for entry in data:
-            if entry["hash"] == hash_val:
-                entry["gameFlags"] |= game_flags
-                break
-        else:
-            data.append({
-                "hash": hash_val,
-                "path": "",
-                "gameFlags": game_flags
-            })
-
-    else:
-        data.append({
-            "hash": hash_val,
-            "path": "",
-            "gameFlags": game_flags
-        })
-
-    with open(json_filename, 'w', newline='\n') as f:
-        json.dump(data, f, indent=2)
+all_data = {}
+for file_name in os.listdir(output_directory):
+    if file_name.endswith(".json"):
+        hash_type = file_name.split('.')[0]
+        all_data[hash_type] = read_json_file(os.path.join(output_directory, file_name))
 
 with open("new_hashes.txt", 'r') as f:
     for line in f:
         hash_with_type, game_flags_str = line.strip().split(':')
         game_flags_str = game_flags_str.split(',')
+        game_flags = sum(GAME_FLAGS[flag_str] for flag_str in game_flags_str)
 
         hash_val, hash_type = infer_type(hash_with_type)
 
         if hash_type:
-            update_json(hash_val, hash_type, game_flags_str)
+            update_data(all_data.setdefault(hash_type, {}), hash_val, game_flags)
+
+for hash_type, data in all_data.items():
+    write_json_file(os.path.join(output_directory, f"{hash_type}.json"), data)
 
 print("Hashes and game flags updated successfully!")
