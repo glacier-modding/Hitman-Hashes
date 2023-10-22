a46f6f636904537fc9b0cfacf01ba674ad8d46cb
Notexe <josh@notex.app>
2023-10-22 13:06:22 +1000

    Only update modified files

--- scripts/add_paths.py
+++ scripts/add_paths.py
@@ -4,18 +4,22 @@ from common_functions import *
 
 output_directory = "paths"
 
+modified_types = set()
+
 def update_data(data, hash_val, path_val):
     for entry in data.values():
         if entry["hash"] == hash_val:
             if ioi_hash(path_val) == hash_val:
                 entry["path"] = path_val.lower()
                 entry.pop("hint", None)
+                modified_types.add(hash_type)
             else:
                 if "path" in entry and entry["path"] != "":
                     print(f"Hash: {hash_val} already has a path {entry['path']}. Skipping addition of hint: {path_val.lower()}.")
                     continue
                 else:
                     entry["hint"] = path_val.lower()
+                    modified_types.add(hash_type)
             break
 
 all_data = {}
@@ -35,6 +39,7 @@ with open("new_paths.txt", 'r') as f:
             update_data(all_data[hash_type], hash_val, path)
 
 for hash_type, data in all_data.items():
-    write_json_file(os.path.join(output_directory, f"{hash_type}.json"), data)
+    if hash_type in modified_types:
+        write_json_file(os.path.join(output_directory, f"{hash_type}.json"), data)
 
 print("Paths added successfully!")
