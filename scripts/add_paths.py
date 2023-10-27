605a75e4f4a689dbf34be2ac50c5b25e90c5be39
Notexe <josh@notex.app>
2023-10-27 10:52:16 +1000

    This slows the script down, commenting out for now

--- scripts/add_paths.py
+++ scripts/add_paths.py
@@ -25,7 +25,7 @@ def update_data(data, hash_val, path_val):
                 modified_types.add(hash_type)
             else:
                 if "path" in entry and entry["path"] != "":
-                    print(f"Hash: {hash_val} already has a path {entry['path']}. Skipping addition of hint: {path_val}.")
+                    # print(f"Hash: {hash_val} already has a path {entry['path']}. Skipping addition of hint: {path_val}.")
                     continue
                 else:
                     if "hint" not in entry or entry["hint"] == "":
