3b7c9d652ba91aa3f0f1709f20da7abed47bae2d
Notexe <josh@notex.app>
2023-10-28 11:43:37 +1000

    Oops.. forgot to change the description

--- scripts/geometry_hints.py
+++ scripts/geometry_hints.py
@@ -4,7 +4,7 @@ import re
 from collections import defaultdict
 import argparse
 
-parser = argparse.ArgumentParser(description="Bruteforce DLGE paths")
+parser = argparse.ArgumentParser(description="Get hints for linked and geometry templates")
 parser.add_argument('--input', type=str, required=False, help="Path to folder containing QN entity.json files.")
 parser.add_argument('--output', type=str, default="new_geometry_hints.txt", help="Output folder and/or name. Defaults to new_geometry_hints.txt.")
 args = parser.parse_args()
