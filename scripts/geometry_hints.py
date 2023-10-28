8a63950664c8b8abae0d36f5feeb2a9b3bd9c87c
Notexe <josh@notex.app>
2023-10-28 11:41:31 +1000

    Update geometry_hints script to use arguments

--- scripts/geometry_hints.py
+++ scripts/geometry_hints.py
@@ -2,15 +2,19 @@ import os
 import json
 import re
 from collections import defaultdict
+import argparse
 
-dir_path = 'H:\\Tools\\rpkg\\QN\\' # QN files must not have any paths in them
+parser = argparse.ArgumentParser(description="Bruteforce DLGE paths")
+parser.add_argument('--input', type=str, required=False, help="Path to folder containing QN entity.json files.")
+parser.add_argument('--output', type=str, default="new_geometry_hints.txt", help="Output folder and/or name. Defaults to new_geometry_hints.txt.")
+args = parser.parse_args()
 
 temp_references = defaultdict(lambda: defaultdict(int))
 
 # Removes numbers from the end of the entity name
 pattern = re.compile(r'(_\d+|\d+)$')
 
-for dirpath, dirnames, filenames in os.walk(dir_path):
+for dirpath, dirnames, filenames in os.walk(args.input):
     for filename in filenames:
         if filename.endswith('.entity.json'):
             full_path = os.path.join(dirpath, filename)
@@ -26,11 +30,12 @@ for dirpath, dirnames, filenames in os.walk(dir_path):
                         factory = entity.get('factory', '')
                         temp_references[factory][name] += 1
 
-# Get the most frequently entity name (should make the hints more accurate)
+# Get the most frequently used entity name (should make the hints more accurate)
 matched_entities = {}
 for factory, names in temp_references.items():
     most_frequent_name = max(names, key=names.get)
     matched_entities[factory] = f"{factory}.TEMP,{most_frequent_name}"
 
-for entity in matched_entities.values():
-    print(entity)
+with open(args.output, "w") as f:
+    for entity in matched_entities.values():
+        f.write(f"{entity}\n")
